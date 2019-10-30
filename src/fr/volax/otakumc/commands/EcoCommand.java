3f9f0fb565fb9bc8274f38d12565da4335e586a1
Volax <47873891+VolaxYT@users.noreply.github.com>
2019-10-30 23:36:40 +0100

    Update eco command

--- src/fr/volax/otakumc/commands/EcoCommand.java
+++ src/fr/volax/otakumc/commands/EcoCommand.java
@@ -21,15 +21,6 @@ public class EcoCommand implements CommandExecutor {
                 sender.sendMessage(OtakuMain.prefix + "§a" + money + "$ ont été ajoutés de " + target.getName() +". Nouveau solde: " + OtakuMain.economy.getBalance(target) + "$");
 
                 return false;
-            }else if(args[0].equalsIgnoreCase("reset") ){
-                if(target == null){ sender.sendMessage(OtakuMain.prefix + "§cCe joueur n'existe pas !"); return false; }
-                if(!OtakuMain.getInstance().isInt(args[2])){  sender.sendMessage(OtakuMain.prefix + "§cVeillez fournir un nombre valide !"); return false; }
-                int money = Integer.parseInt(args[2]);
-                OtakuMain.economy.withdrawPlayer(target, OtakuMain.economy.getBalance(target));
-                target.sendMessage(OtakuMain.prefix + "§aVotre solde a été modifié à " + OtakuMain.economy.getBalance(target) + "$.");
-                sender.sendMessage(OtakuMain.prefix + "§aVous avez modifié à " + OtakuMain.economy.getBalance(target) + "$ le solde de " + target.getName());
-
-                return false;
             }else if(args[0].equalsIgnoreCase("set")){
                 if(target == null){ sender.sendMessage(OtakuMain.prefix + "§cCe joueur n'existe pas !"); return false; }
                 if(!OtakuMain.getInstance().isInt(args[2])){  sender.sendMessage(OtakuMain.prefix + "§cVeillez fournir un nombre valide !"); return false; }
@@ -40,11 +31,25 @@ public class EcoCommand implements CommandExecutor {
                 if(!OtakuMain.getInstance().isInt(args[2])){  sender.sendMessage(OtakuMain.prefix + "§cVeillez fournir un nombre valide !"); return false; }
 
                 return false;
+            }else{
+                sender.sendMessage(OtakuMain.prefix + "§7/otakueco <give|reset|set|take> <joueur> <montant>");
+                return false;
+            }
+        }else if(args.length == 2){
+            Player target = Bukkit.getPlayer(args[1]);
+            if(args[0].equalsIgnoreCase("reset") ){
+                if(target == null){ sender.sendMessage(OtakuMain.prefix + "§cCe joueur n'existe pas !"); return false; }
+                OtakuMain.economy.withdrawPlayer(target, OtakuMain.economy.getBalance(target));
+                target.sendMessage(OtakuMain.prefix + "§aVotre solde a été modifié à " + OtakuMain.economy.getBalance(target) + "$.");
+                sender.sendMessage(OtakuMain.prefix + "§aVous avez modifié à " + OtakuMain.economy.getBalance(target) + "$ le solde de " + target.getName());
+                return false;
+            }else{
+                sender.sendMessage(OtakuMain.prefix + "§7/otakueco <give|reset|set|take> <joueur> <montant>");
+                return false;
             }
         }else{
             sender.sendMessage(OtakuMain.prefix + "§7/otakueco <give|reset|set|take> <joueur> <montant>");
             return false;
         }
-        return false;
     }
 }
